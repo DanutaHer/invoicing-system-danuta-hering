c57a816479c7a28349b6a33a0c887397d8c1751c
DanutaHer <hering.danutaa@gmail.com>
2023-07-14 13:11:37 +0200

    Created REST services tests

---
+++ app/src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
@@ -0,0 +1,59 @@
+package pl.futurecollars.invoicing.db.memory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import lombok.Data;
+import pl.futurecollars.invoicing.db.Database;
+import pl.futurecollars.invoicing.model.Invoice;
+
+@Data
+public class InMemoryDatabase implements Database {
+
+  private Map<Integer, Invoice> memoryDatabase = new HashMap<>();
+  private int count = 1;
+
+  @Override
+  public int save(Invoice invoice) {
+    this.memoryDatabase.put(count, invoice);
+    invoice.setId(count);
+    return count++;
+  }
+
+  @Override
+  public Optional<Invoice> getByID(int id) {
+    printIllegalArgumentException(id);
+    return memoryDatabase.values()
+        .stream()
+        .filter(invoice -> invoice == memoryDatabase.get(id))
+        .findAny();
+  }
+
+  @Override
+  public List<Invoice> getAll() {
+    return memoryDatabase.values()
+        .stream()
+        .toList();
+  }
+
+  @Override
+  public Optional<Invoice> update(int id, Invoice updatedInvoice) {
+    printIllegalArgumentException(id);
+    memoryDatabase.replace(id, memoryDatabase.get(id), updatedInvoice);
+    updatedInvoice.setId(id);
+    return Optional.of(updatedInvoice);
+  }
+
+  @Override
+  public Optional<Invoice> delete(int id) {
+    printIllegalArgumentException(id);
+    return Optional.ofNullable(memoryDatabase.remove(id));
+  }
+
+  private void printIllegalArgumentException(int id) {
+    if (id <= 0) {
+      throw new IllegalArgumentException("Error: id cannot be negative");
+    }
+  }
+}
---
+++ app/src/test/groovy/pl/futurecollars/invoicing/service/InvoiceServiceTest.groovy
@@ -0,0 +1,58 @@
+package pl.futurecollars.invoicing.service
+
+import pl.futurecollars.invoicing.db.Database
+import pl.futurecollars.invoicing.model.Invoice
+import spock.lang.Specification
+
+class InvoiceServiceTest extends Specification {
+
+    private Database database = Mock()
+    private InvoiceService service = new InvoiceService(database)
+
+    def "shouldSave"() {
+        given:
+        def invoice = new Invoice()
+
+        when:
+        service.save(invoice)
+
+        then:
+        1 * database.save(invoice)
+    }
+
+    def "shouldGetByID"() {
+        when:
+        service.getByID(2)
+
+        then:
+        1 * database.getByID(2)
+    }
+
+    def "shouldGetAll"() {
+        when:
+        service.getAll()
+
+        then:
+        1 * database.getAll()
+    }
+
+    def "shouldUpdate"() {
+        given:
+        def invoice = new Invoice()
+
+        when:
+        service.update(2, invoice)
+
+        then:
+        1 * database.update(2, invoice)
+    }
+
+    def "shouldDelete"() {
+        when:
+        service.delete(2)
+
+        then:
+        1 * database.delete(2)
+
+    }
+}
