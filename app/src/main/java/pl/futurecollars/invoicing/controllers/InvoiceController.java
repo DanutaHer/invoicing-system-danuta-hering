d2d661baa28afff5b8491a317f76b549a0e6fece
DanutaHer <hering.danutaa@gmail.com>
2023-07-24 19:44:30 +0200

    Added Swagger

--- app/src/main/java/pl/futurecollars/invoicing/controllers/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controllers/InvoiceController.java
@@ -2,44 +2,33 @@ package pl.futurecollars.invoicing.controllers;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
+@AllArgsConstructor
 @RestController
-@RequestMapping("invoices")
-public class InvoiceController {
+public class InvoiceController implements InvoiceApi {
 
   private final InvoiceService invoiceService;
 
-  @Autowired
-  public InvoiceController(InvoiceService invoiceService) {
-    this.invoiceService = invoiceService;
-  }
-
-  @PostMapping
+  @Override
   public ResponseEntity<Integer> add(@RequestBody Invoice invoice) {
     return ResponseEntity.ok(invoiceService.save(invoice));
   }
 
-  @GetMapping
+  @Override
   public ResponseEntity<List<Invoice>> getAll() {
     List<Invoice> allInvoices = invoiceService.getAll();
     return ResponseEntity.ok(allInvoices);
-
   }
 
-  @GetMapping("/{id}")
+  @Override
   public ResponseEntity<Optional<Invoice>> getExactInvoice(@PathVariable("id") int id) {
     Optional<Invoice> optionalInvoice = invoiceService.getByID(id);
     if (optionalInvoice.isPresent()) {
@@ -49,7 +38,7 @@ public class InvoiceController {
     }
   }
 
-  @PutMapping("/{id}")
+  @Override
   public ResponseEntity<Optional<Invoice>> update(@PathVariable("id") int id, @RequestBody Invoice invoice) {
     Optional<Invoice> optionalInvoice = invoiceService.update(id, invoice);
     if (optionalInvoice.isPresent()) {
@@ -59,7 +48,7 @@ public class InvoiceController {
     }
   }
 
-  @DeleteMapping("/{id}")
+  @Override
   public ResponseEntity<Optional<Invoice>> delete(@PathVariable("id") int id) {
     Optional<Invoice> optionalInvoice = invoiceService.delete(id);
     if (optionalInvoice.isPresent()) {
