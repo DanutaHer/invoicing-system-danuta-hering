325685621060c4bd3d3795e1bfff35341407df23
Szymon WB <szymon.jedynak@gmail.com>
2023-07-24 20:28:11 +0200

    update InvoiceController.java

--- app/src/main/java/pl/futurecollars/invoicing/controllers/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controllers/InvoiceController.java
@@ -21,57 +21,56 @@ import pl.futurecollars.invoicing.service.InvoiceService;
 @RequestMapping("invoice")
 @Slf4j
 public class InvoiceController {
+    private final InvoiceService invoiceService;
 
-  private final InvoiceService invoiceService;
-
-  @Autowired
-  public InvoiceController(InvoiceService invoiceService) {
-    this.invoiceService = invoiceService;
-  }
-
-  @PostMapping
-  public ResponseEntity<Integer> add(@RequestBody Invoice invoice) {
-    invoiceService.save(invoice);
-    return ResponseEntity.status(HttpStatus.CREATED).build();
-  }
-
-  @GetMapping
-  public ResponseEntity<List<Invoice>> getAll() {
-    List<Invoice> allInvoices = invoiceService.getAll();
-    return ResponseEntity.ok(allInvoices);
-
-  }
-
-  @GetMapping("/{id}")
-  public ResponseEntity<Optional<Invoice>> getExactInvoice(@PathVariable("id") int id) {
-    Optional<Invoice> optionalInvoice = invoiceService.getByID(id);
-    if (optionalInvoice.isPresent()) {
-      log.info("Get invoice: " + optionalInvoice);
-      return ResponseEntity.ok(optionalInvoice);
-    } else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    @Autowired
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
     }
-  }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<Optional<Invoice>> update(@PathVariable("id") int id, @RequestBody Invoice invoice) {
-    Optional<Invoice> optionalInvoice = invoiceService.update(id, invoice);
-    if (optionalInvoice.isPresent()) {
-      log.info("Update invoice: " + optionalInvoice);
-      return ResponseEntity.ok(optionalInvoice);
-    } else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    @PostMapping
+    public ResponseEntity<Integer> add(@RequestBody Invoice invoice) {
+        invoiceService.save(invoice);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-  }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<Optional<Invoice>> delete(@PathVariable("id") int id) {
-    Optional<Invoice> optionalInvoice = invoiceService.delete(id);
-    if (optionalInvoice.isPresent()) {
-      log.info("Delete invoice: " + optionalInvoice);
-      return ResponseEntity.ok(optionalInvoice);
-    } else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    @GetMapping
+    public ResponseEntity<List<Invoice>> getAll() {
+        List<Invoice> allInvoices = invoiceService.getAll();
+        return ResponseEntity.ok(allInvoices);
+
     }
-  }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Invoice>> getExactInvoice(@PathVariable("id") int id) {
+        Optional<Invoice> optionalInvoice = invoiceService.getByID(id);
+        if (optionalInvoice.isPresent()) {
+            log.info("Get invoice: " + optionalInvoice);
+            return ResponseEntity.ok(optionalInvoice);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<Invoice>> update(@PathVariable("id") int id, @RequestBody Invoice invoice) {
+        Optional<Invoice> optionalInvoice = invoiceService.update(id, invoice);
+        if (optionalInvoice.isPresent()) {
+            log.info("Update invoice: " + optionalInvoice);
+            return ResponseEntity.ok(optionalInvoice);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Optional<Invoice>> delete(@PathVariable("id") int id) {
+        Optional<Invoice> optionalInvoice = invoiceService.delete(id);
+        if (optionalInvoice.isPresent()) {
+            log.info("Delete invoice: " + optionalInvoice);
+            return ResponseEntity.ok(optionalInvoice);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
