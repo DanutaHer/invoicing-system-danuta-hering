92352064a0aa7d5dbf3a82637eb6bc04849362c5
DanutaHer <61617084+DanutaHer@users.noreply.github.com>
2023-08-03 19:43:43 +0200

    Task 8/configure swagger UI (#27)
    * Created REST services tests
    
    * Task 7/Changed database depending on passed active profile
    
    * Corrected changes from master
    
    * Corrected DatabaseConfiguration
    
    * Corrected InvoiceService
    
    * Refactoring
    
    * Refactoring
    
    * First step of configuration
    
    * Added Swagger
    
    * Corrected config folder name
    
    * Corrected value in Invoice class

--- app/src/main/java/pl/futurecollars/invoicing/controllers/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controllers/InvoiceController.java
@@ -2,47 +2,36 @@ package pl.futurecollars.invoicing.controllers;
 
 import java.util.List;
 import java.util.Optional;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
+@AllArgsConstructor
 @RestController
-@RequestMapping("invoices")
 @Slf4j
-public class InvoiceController {
+public class InvoiceController implements InvoiceApi {
 
   private final InvoiceService invoiceService;
 
-  @Autowired
-  public InvoiceController(InvoiceService invoiceService) {
-    this.invoiceService = invoiceService;
-  }
-
-  @PostMapping
+  @Override
   public ResponseEntity<Integer> add(@RequestBody Invoice invoice) {
     return ResponseEntity.ok(invoiceService.save(invoice));
   }
 
-  @GetMapping
+  @Override
   public ResponseEntity<List<Invoice>> getAll() {
     List<Invoice> allInvoices = invoiceService.getAll();
     log.info("Get the count of all invoices: " + allInvoices.size());
     return ResponseEntity.ok(allInvoices);
-
   }
 
-  @GetMapping("/{id}")
+  @Override
   public ResponseEntity<Optional<Invoice>> getExactInvoice(@PathVariable("id") int id) {
     Optional<Invoice> optionalInvoice = invoiceService.getByID(id);
     if (optionalInvoice.isPresent()) {
@@ -53,7 +42,7 @@ public class InvoiceController {
     }
   }
 
-  @PutMapping("/{id}")
+  @Override
   public ResponseEntity<Optional<Invoice>> update(@PathVariable("id") int id, @RequestBody Invoice invoice) {
     Optional<Invoice> optionalInvoice = invoiceService.update(id, invoice);
     if (optionalInvoice.isPresent()) {
@@ -64,7 +53,7 @@ public class InvoiceController {
     }
   }
 
-  @DeleteMapping("/{id}")
+  @Override
   public ResponseEntity<Optional<Invoice>> delete(@PathVariable("id") int id) {
     Optional<Invoice> optionalInvoice = invoiceService.delete(id);
     if (optionalInvoice.isPresent()) {
--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Company {
 
+  @ApiModelProperty(value = "Company name", required = true, example = "Global LTD")
   private String name;
+  @ApiModelProperty(value = "Company address", required = true, example = "ul. Zlota 12/3")
   private String address;
+  @ApiModelProperty(value = "Company tax identification number", required = true, example = "123456789")
   private String taxIdentificationNumber;
 }
