123b2711ad6dcf9b9e1df6bd443666b7f4d00f0c
DanutaHer <hering.danutaa@gmail.com>
2023-08-14 15:30:52 +0200

    Add setup SQL with Flyway + changed TestHelper to builder pattern

--- app/src/test/groovy/pl/futurecollars/invoicing/TestHelper.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/TestHelper.groovy
@@ -11,14 +11,29 @@ import static pl.futurecollars.invoicing.model.Vat.VAT_5
 class TestHelper {
 
     static company(int id) {
-        new Company("Name $id", "ul. Green", "123456789");
+        Company.builder()
+                .name("Name $id")
+                .address("ul. Zielona 17, 01-240 Warszawa")
+                .taxIdentificationNumber("123-45-67-89")
+                .build()
     }
 
     static product(int id) {
-        new InvoiceEntry("Descr $id", BigDecimal.ONE, BigDecimal.ONE, VAT_5);
+        InvoiceEntry.builder()
+                .description("Descr $id")
+                .price(BigDecimal.ONE)
+                .vatValue(BigDecimal.ONE)
+                .vatRate(VAT_5)
+                .build();
     }
 
     static invoice(int id) {
-        new Invoice(id, LocalDate.now(), company(id), company(id), List.of(product(id)));
+        Invoice.builder()
+                .id(id)
+                .date(LocalDate.now())
+                .buyer(company(id))
+                .seller(company(id))
+                .entries(List.of(product(id)))
+                .build();
     }
 }
