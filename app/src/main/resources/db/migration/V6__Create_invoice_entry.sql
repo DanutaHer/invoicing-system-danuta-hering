bd8abadf7ec7f9cda7ceb4fd79d59bf38483c52c
DanutaHer <hering.danutaa@gmail.com>
2023-08-14 15:30:52 +0200

    Add setup SQL with Flyway + changed TestHelper to builder pattern

---
+++ app/src/main/resources/db/migration/V6__Create_invoice_entry.sql
@@ -0,0 +1,18 @@
+CREATE TABLE public.invoice_entry
+(
+    id bigserial NOT NULL,
+    description character varying(200) NOT NULL,
+    price numeric(10, 2) NOT NULL DEFAULT 0,
+    vat_value numeric(10, 2) NOT NULL DEFAULT 0,
+    vat_rate bigint NOT NULL,
+    expense_related_to_car bigint NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public.invoice_entry
+    ADD CONSTRAINT vat_rate_fk FOREIGN KEY (vat_rate)
+        REFERENCES public.vat (id);
+
+ALTER TABLE public.invoice_entry
+    ADD CONSTRAINT car_fk FOREIGN KEY (expense_related_to_car)
+        REFERENCES public.car (id);
