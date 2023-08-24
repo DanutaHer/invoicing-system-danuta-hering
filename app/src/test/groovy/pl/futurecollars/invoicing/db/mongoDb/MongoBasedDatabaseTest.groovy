3021b2ca3d1221b5026f0a10e9d833bec91c24d9
DanutaHer <hering.danutaa@gmail.com>
2023-08-24 20:14:46 +0200

    Added MongoDB database

---
+++ app/src/test/groovy/pl/futurecollars/invoicing/db/mongoDb/MongoBasedDatabaseTest.groovy
@@ -0,0 +1,20 @@
+package pl.futurecollars.invoicing.db.mongoDb
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.IfProfileValue
+import pl.futurecollars.invoicing.db.AbstractDatabaseTest
+import pl.futurecollars.invoicing.db.Database
+
+@IfProfileValue(name = "spring.profiles.active", value = "mongo")
+class MongoBasedDatabaseTest extends AbstractDatabaseTest {
+
+    @Autowired
+    private MongoBasedDatabase mongoBasedDatabase
+
+    @Override
+    Database getDatabaseInstance() {
+        assert mongoBasedDatabase != null
+        mongoBasedDatabase
+    }
+}
