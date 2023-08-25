d4639af0c9e30b9d8e55addc3099336c54d9b5a5
DanutaHer <hering.danutaa@gmail.com>
2023-08-25 08:23:55 +0200

    Refactoring

--- app/src/test/groovy/pl/futurecollars/invoicing/db/memory/InMemoryDatabaseTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/db/memory/InMemoryDatabaseTest.groovy
@@ -1,9 +1,10 @@
 package pl.futurecollars.invoicing.db.memory
 
-
+import org.springframework.test.annotation.IfProfileValue
 import pl.futurecollars.invoicing.db.AbstractDatabaseTest
 import pl.futurecollars.invoicing.db.Database
 
+@IfProfileValue(name = "spring.profiles.active", value = "memory")
 class InMemoryDatabaseTest extends AbstractDatabaseTest {
 
     @Override
