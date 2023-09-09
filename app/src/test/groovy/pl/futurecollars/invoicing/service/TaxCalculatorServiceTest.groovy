94adff5b3e0a3478f1029407907a66f87239400a
DanutaHer <hering.danutaa@gmail.com>
2023-09-09 12:08:32 +0200

    Refactoring TaxCalculatorServiceTest

--- app/src/test/groovy/pl/futurecollars/invoicing/service/TaxCalculatorServiceTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/service/TaxCalculatorServiceTest.groovy
@@ -2,57 +2,115 @@ package pl.futurecollars.invoicing.service
 
 import pl.futurecollars.invoicing.TestHelper
 import pl.futurecollars.invoicing.db.Database
+import pl.futurecollars.invoicing.model.Invoice
 import spock.lang.Specification
 
 class TaxCalculatorServiceTest extends Specification {
-    private Database database = Mock()
+
+    private Database<Invoice> database = Mock()
     private TaxCalculatorService taxService = new TaxCalculatorService(database)
 
     def "shouldGetIncome"() {
         given:
         def invoice = TestHelper.invoice(1)
         def invoiceIdentificationNumber = invoice.getSeller().taxIdentificationNumber
+        database.getAll() >> List.of(invoice)
 
         when:
-        taxService.income(invoiceIdentificationNumber)
+        def expectedIncome = taxService.income(invoiceIdentificationNumber)
 
         then:
-        1 * database.visit(_, _)
+        expectedIncome == 1500
     }
 
     def "shouldGetCosts"() {
         given:
         def invoice = TestHelper.invoice(1)
         def invoiceIdentificationNumber = invoice.getSeller().taxIdentificationNumber
+        database.getAll() >> List.of(invoice)
 
         when:
-        taxService.costs(invoiceIdentificationNumber)
+        def expectedCosts = taxService.costs(invoiceIdentificationNumber)
 
         then:
-        1 * database.visit(_, _)
+        expectedCosts == 1657.5
     }
 
     def "shouldGetIncomingVat"() {
         given:
         def invoice = TestHelper.invoice(1)
         def invoiceIdentificationNumber = invoice.getSeller().taxIdentificationNumber
+        database.getAll() >> List.of(invoice)
 
         when:
-        taxService.incomingVat(invoiceIdentificationNumber)
+        def expectedIncomingVat = taxService.incomingVat(invoiceIdentificationNumber)
 
         then:
-        1 * database.visit(_, _)
+        expectedIncomingVat == 315
     }
 
     def "shouldGetOutgoingVat"() {
         given:
         def invoice = TestHelper.invoice(1)
         def invoiceIdentificationNumber = invoice.getSeller().taxIdentificationNumber
+        database.getAll() >> List.of(invoice)
 
         when:
-        taxService.outgoingVat(invoiceIdentificationNumber)
+        def expectedOutgoingVat = taxService.outgoingVat(invoiceIdentificationNumber)
 
         then:
-        1 * database.visit(_, _)
+        expectedOutgoingVat == 157.5
+    }
+
+    def "shouldGetEarnings"() {
+        given:
+        def invoice = TestHelper.invoice(1)
+        def invoiceIdentificationNumber = invoice.getSeller().taxIdentificationNumber
+        database.getAll() >> List.of(invoice)
+
+        when:
+        def expectedEarnings = taxService.getEarnings(invoiceIdentificationNumber)
+
+        then:
+        expectedEarnings == -157.5
+    }
+
+    def "shouldGetVatToPay"() {
+        given:
+        def invoice = TestHelper.invoice(1)
+        def invoiceIdentificationNumber = invoice.getSeller().taxIdentificationNumber
+        database.getAll() >> List.of(invoice)
+
+        when:
+        def expectedVatToPay = taxService.getVatToPay(invoiceIdentificationNumber)
+
+        then:
+        expectedVatToPay == 157.5
+    }
+
+    def "shouldCalculateTaxes"() {
+        given:
+        def invoice = TestHelper.invoice(1)
+        def company = invoice.getSeller()
+        database.getAll() >> List.of(invoice)
+
+        when:
+        def expectedCalculateTaxes = taxService.calculateTaxes(company)
+
+        then:
+        expectedCalculateTaxes.income == 1500
+        expectedCalculateTaxes.costs == 1657.5
+        expectedCalculateTaxes.incomeMinusCosts == -157.5
+        expectedCalculateTaxes.pensionInsurance == 514.57
+        expectedCalculateTaxes.incomeMinusCostsMinusPensionInsurance == -672.07
+        expectedCalculateTaxes.incomeMinusCostsMinusPensionInsuranceRounded == -672
+        expectedCalculateTaxes.incomeTax == -127.68
+        expectedCalculateTaxes.healthInsurancePaid == 319.94
+        expectedCalculateTaxes.healthInsuranceToSubtract == 275.5
+        expectedCalculateTaxes.incomeTaxMinusHealthInsurance == -403.18
+        expectedCalculateTaxes.finalIncomeTax == -403
+        expectedCalculateTaxes.incomingVat == 315
+        expectedCalculateTaxes.outgoingVat == 157.5
+        expectedCalculateTaxes.vatToPay == 157.5
     }
 }
